d95c12eb6ee2d7cd643728cea136673200f3d2d6
chun77 <zichunwang733@gmail.com>
2024-09-25 01:18:08 -0400

    ci path

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,7 +1,7 @@
-from src.main import pandas_summary, polars_summary
+from script import pandas_summary, polars_summary
 
 def test_pandas_summary():
-    assert pandas_summary('data/medals_total.csv') is not None
+    assert pandas_summary('../data/medals_total.csv') is not None
 
 def test_polars_summary():
-    assert polars_summary('data/medals_total.csv') is not None
+    assert polars_summary('../data/medals_total.csv') is not None
